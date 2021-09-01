6919057a3aed64a172dfcb749792118023003dd8
Anjato <atolkinen@gmail.com>
2021-09-01 02:25:10 -0500

    replaced unicode mathematical operators with programmable operators; updated link to use the maximum settings except for absolute value since I'm not sure how to replace | | with abs() around the correct integers right now

--- bigBrain/main.py
+++ bigBrain/main.py
@@ -3,19 +3,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pyautogui
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get("https://www.thatquiz.org/tq-1/math/arithmetic/")
-table_equation = browser.find_element_by_xpath("//table[@class='bZQ q9av']/tbody/tr/td[2]")
+browser.get("https://www.thatquiz.org/tq-1/?-j1gh8f-l34-mpnv600-n35-p0")
 
+# grabs cell value in table where the equation is found
+table_equation = browser.find_element_by_xpath("//table[@class='g6cd']/tbody/tr/td[2]/table/tbody/tr/td[1]")
 
-# table_settings = browser.find_element_by_xpath("//table[@class='bZQ q9av']/tbody/tr/td[1]")
-# if True:
-#     print(table_settings.text)
-# maybe able to manipulate the settings automatically when opening the web page? doubtful though
 
 while True:
-    answer = str(int(eval(table_equation.text[-14:-7])))
+    # fixes stale element reference error (somehow, no clue why tbh)
+    table_equation = browser.find_element_by_xpath("//table[@class='g6cd']/tbody/tr/td[2]/table/tbody/tr/td[1]")
 
-    print(table_equation.text[-14:-7])
+    t = table_equation.text.replace(u"\u00D7", "*").replace(u"\u00F7", "/").replace(u"\u2013", "-")
+
+    print(t)
+    answer = str(int(eval(t)))
     print(answer)
 
     pyautogui.write(answer, _pause=False)
