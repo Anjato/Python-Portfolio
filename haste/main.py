35ced736e65d9ef5b02e9ddf6713a9e05ff403d0
Anjato <atolkinen@gmail.com>
2021-09-01 04:45:02 -0500

    fancy pantsy print

--- haste/main.py
+++ haste/main.py
@@ -1,10 +1,29 @@
 import pyautogui
-import time
+from time import sleep
+import os
+
+clear = lambda: os.system('cls')
 
 file_name = input('File name:')
 file_handle = open(file_name)
 
-time.sleep(5)
+print("Starting in 5 seconds...")
+sleep(1)
+clear()
+print("Starting in 4 seconds...")
+sleep(1)
+clear()
+print("Starting in 3 seconds...")
+sleep(1)
+clear()
+print("Starting in 2 seconds...")
+sleep(1)
+clear()
+print("Starting in 1 seconds...")
+sleep(1)
+clear()
+print("Starting! :)")
+
 
 for word in file_handle:
     pyautogui.typewrite(word, 0.0)
