c0659e40499e41e4fa901a2c69661a785cd45ca3
Anjato <atolkinen@gmail.com>
2021-11-07 17:34:14 -0600

    updated

--- bigBrain/main.py
+++ bigBrain/main.py
@@ -5,8 +5,8 @@ import pyautogui
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://www.thatquiz.org/tq-1/?-j1gh8f-l34-mpnv600-n35-p0")
 
-
 while True:
+
     # grabs cell value in table where the equation is found
     # originally had as a global but it would throw a stale element reference error, idk why
     table_equation = browser.find_element_by_xpath("//table[@class='g6cd']/tbody/tr/td[2]/table/tbody/tr/td[1]")
