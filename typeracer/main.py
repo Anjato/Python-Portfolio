b587de8d55ee7b662cb41018ef12000e1d88e0e5
Anjato <atolkinen@gmail.com>
2022-10-25 17:46:45 -0500

    fixed with new website updates

--- typeracer/main.py
+++ typeracer/main.py
@@ -2,26 +2,18 @@ import time
 import pyautogui
 import cv2
 import pytesseract
-import tkinter as tk
+from pathlib import Path
+import os
 from cryptography.fernet import Fernet
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-
-def getRes():
-    width, height = pyautogui.size()
-    global modW
-    global modH
-    modW = width / 1.2
-    modH = height / 1.2
-
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 driver.get('https://play.typeracer.com/')
-# driver.maximize_window()
-getRes()
-driver.set_window_size(modW, modH)
 
 # give page time to load the sign in javascript in top right
 time.sleep(2)
@@ -30,23 +22,39 @@ time.sleep(2)
 # function to login the user with credentials found in creds.txt (same folder as main.py), going to add encryption
 # support to not store password in plaintext
 def login():
-    # file with login details
-    f = open("creds.txt", "r")
-    lines = f.readlines()
-    username = lines[0]
-    password = lines[1]
-    f.close()
+    file = "creds.txt"
+
+    if not os.path.exists(file):
+        user = str(input("Please enter username:"))
+        passwd = str(input("Please enter password:"))
+        f = open(file, "w")
+        f.writelines([user, "\n", passwd])
+        f.close()
+    elif os.stat(file).st_size == 0:
+        user = str(input("Please enter username:"))
+        passwd = str(input("Please enter password:"))
+        f = open(file, "w")
+        f.writelines([user, "\n", passwd])
+        f.close()
+    else:
+        f = open(file, "r")
+        read = f.readlines()
+        user = read[0]
+        passwd = read[1]
+        f.close()
+
+############# ADD FOCUS WINDOW HERE #######################
 
     # sign in button in top right
-    loginbtn = driver.find_element_by_xpath('//*[@id="userInfo"]/div/div[2]/div[1]/div[2]/a[2]')
+    loginbtn = driver.find_element(By.XPATH, "//a[@class='promptBtn signIn']")
     loginbtn.click()
     # give time for form to pop up
     time.sleep(1)
     # replaces new line to prevent hitting enter after entering username
-    pyautogui.write(username.replace("\n", ""), interval=0.01)
+    pyautogui.write(user.replace("\n", ""), interval=0.01)
     # hits tab to goto password field
     pyautogui.press('tab')
-    pyautogui.write(password, interval=0.01)
+    pyautogui.write(passwd, interval=0.01)
     pyautogui.press('enter')
 
     # give time for sign in form to disappear
@@ -57,30 +65,50 @@ def login():
 
 # main function that complete races. must be on the main page for the function to find the begin race button
 def main():
-    race = driver.find_element_by_xpath('//*[@id="gwt-uid-1"]/a')
+    race = driver.find_element(By.XPATH, "//a[@class='gwt-Anchor prompt-button bkgnd-green']")
     race.click()
 
     # gives time for xpath to load and be found while running the loop statement(s)
     time.sleep(4)
 
-    go = driver.find_element_by_class_name('lightLabel')
+    go = driver.find_element(By.XPATH, "//div[@class='gameStatusLabel']")
     compare = "Go!"
 
-    letters = driver.find_element_by_xpath('//*[@id="gwt-uid-21"]/table/tbody/tr[2]/td/table/tbody/tr['
-                                           '1]/td/table/tbody/tr[1]/td/div/div' or
-                                           '//*[@id="gwt-uid-25"]/table/tbody/tr[2]/td/table/tbody/tr['
-                                           '1]/td/table/tbody/tr[1]/td/div/div' or
-                                           '//*[@id="gwt-uid-29"]/table/tbody/tr[2]/td/table/tbody/tr['
-                                           '1]/td/table/tbody/tr[1]/td')
+    changeDisplayFormat = driver.find_element(By.XPATH, "//a[@class='gwt-Anchor display-format-trigger']")
+
+    changeDisplayFormat.click()
+    time.sleep(0.5)
+    oldStyleOneRadio = driver.find_element(By.XPATH, "//span[@class='gwt-RadioButton OLD_FULLTEXT']/input")
+    oldStyleOneRadio.click()
+    time.sleep(0.5)
+    changeDisplayFormatX = driver.find_element(By.XPATH, "//div[@class='xButton']")
+    changeDisplayFormatX.click()
+    time.sleep(1)
+
+    words = driver.find_element(By.XPATH, "//div[@class='nonHideableWords unselectable']/span[2]")
+    comma = driver.find_element(By.XPATH, "//div[@class='nonHideableWords unselectable']/span[3]")
+
+    print(go.text)
     while go.text != compare:
         time.sleep(0.1)
-        print(go.text)
 
     while go.text == compare:
-        pyautogui.write(letters.text, interval=0.05)
-        print(letters.text)
+        comma = driver.find_element(By.XPATH, "//div[@class='nonHideableWords unselectable']/span[3]")
+        if comma.text == ',':
+            print(words.text + comma.text)
+            pyautogui.write(words.text + ',' + ' ', interval=0.05)
+        else:
+            print(words.text + comma.text)
+            pyautogui.write(words.text + ' ', interval=0.05)
 
-        response()
+    # if go.text == compare:
+    #     pyautogui.write(firstWord.text + ' ', interval=0.05)
+    #     print(firstWord.text)
+    #     while go.text == compare:
+    #         pyautogui.write(otherWords.text, interval=0.05)
+    #         print(otherWords.text)
+
+    response()
 
 
 # function to complete the typing speed verification. not currently done due to tesseract not reading images well
@@ -117,3 +145,17 @@ def tesseracttest():
 
     text = pytesseract.image_to_string(img)
     print(text)
+
+
+def getRes():
+    width, height = pyautogui.size()
+    global modW
+    global modH
+    modW = width / 1.2
+    modH = height / 1.2
+    driver.set_window_size(modW, modH)
+
+    login()
+
+
+getRes()
